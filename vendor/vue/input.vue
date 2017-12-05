1d60a6a2360b25ee143f9a0744c2d5b2d570e4a0
viclm <xliuming@outlook.com>
2017-12-05 21:21:00 +0800

    abstract input from vue component

--- vendor/vue/input.vue
+++ vendor/vue/input.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="numeric-input" :class="{ readonly: readonly, disabled: disabled }" @touchend="focus">
+  <div class="numeric-input" :class="{ readonly: readonly, disabled: disabled }" @touchend="onFocus">
     <input type="hidden" :name="name" :value="value" />
-    <div>
-      <span class="numeric-input-placeholder" v-if="rawValue.length === 0">{{placeholder}}</span><span v-if="rawValue.length" v-for="(c, index) in rawValue" :data-index="index + 1">{{c}}</span><i v-if="cursorTimer" v-show="cursorVisible" :style="{backgroundColor: cursorColor}"></i>
+    <div v-if="state">
+      <span class="numeric-input-placeholder" v-if="state.rawValue.length === 0">{{placeholder}}</span><span v-for="(c, i) in state.rawValue" :key="i" :data-index="i + 1">{{c}}</span><i v-if="state.cursorTimer" v-show="state.cursorVisible" :style="{backgroundColor: state.cursorColor}"></i>
     </div>
   </div>
 </template>
@@ -11,307 +11,74 @@
 
 import Vue from 'vue'
 import Keyboard from './keyboard.vue'
-
-const requestAnimationFrame = window.requestAnimationFrame || window.setTimeout
-
-const animation = function (iterable, done = () => {}, frames = 60) {
-  let running = true
-  let frame = 0
-  function closure(timestamp) {
-    if (!running) { return }
-    iterable(timestamp, ++frame, frames)
-    if (frame < frames) {
-      requestAnimationFrame(closure, 0)
-    }
-    else {
-      done()
-    }
-  }
-  requestAnimationFrame(closure, 0)
-  return () => {
-    running = false
-  }
-}
-
-
-const keyboardCenter = (function () {
-
-  let activeInput
-
-  return {
-    open(input) {
-      this.close()
-      activeInput = input
-      document.addEventListener('touchend', this.close, false)
-    },
-    close() {
-      if (!activeInput) {
-        return
-      }
-      activeInput.closeKeyboard()
-      activeInput = null
-      document.removeEventListener('touchend', this.close, false)
-    }
-  }
-
-})()
+import { Options, Mixins } from 'lib/input'
 
 export default {
-  props: {
-    type: {
-      type: String,
-      default: 'number'
-    },
-    autofocus: {
-      type: Boolean,
-      default: false
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    maxlength: {
-      type: Number
-    },
-    name: {
-      type: String
-    },
-    placeholder: {
-      type: String
-    },
-    readonly: {
-      type: Boolean,
-      default: false
-    },
-    value: {
-      type: [String, Number]
-    },
-    format: {
-      type: [String, Function]
-    },
-    keyboard: {
-      type: Object
+  mixins: [{
+    methods: Mixins
+  }],
+  props: (function () {
+    let props = {}
+    for (let name in Options) {
+      props[name] = {
+        default: Options[name]
+      }
     }
-  },
+    return props
+  })(),
   data() {
     return {
-      rawValue: [],
-      cursorPos: 0,
-      cursorVisible: false,
-      cursorColor: null,
-      cursorTimer: null
+      props: null,
+      state: null
     }
   },
   created() {
-    if (typeof this.format === 'function') {
-      this._validate = (val) => {
-        return this.format(val.join(''))
-      }
-    }
-    else if (typeof this.format === 'string') {
-      this._validate = (val) => {
-        const reg = new RegExp(this.format)
-        return reg.test(val.join(''))
-      }
-    }
-    else {
-      this._validate = val => true
-    }
+    this.constructor(this._props)
   },
   mounted() {
-    if (this.autofocus && !this.readonly && !this.disabled) {
-      this.openKeyboard()
-    }
-    this.cursorColor = window.getComputedStyle(this.$el).getPropertyValue('color')
+    this.onMounted(this.$el)
   },
   beforeDestroy() {
-    window.clearInterval(this.cursorTimer)
-    if (this._keyboard) {
-      this._keyboard.$destroy()
-    }
+    this.destructor()
   },
   watch: {
     value: {
-      immediate: true,
       handler(val) {
-        if (this.toValue(this.rawValue) === val) {
+        let current = this.state.rawValue.join('')
+        if (current === val || parseFloat(current, 10) === val) {
           return
         }
-        if (val != null) {
-          this.rawValue = val.toString().split('')
-        }
-        else {
-          this.rawValue = []
-        }
+        this.setValue(val)
       }
-    },
-    rawValue(value) {
-      this.$emit('input', this.toValue(value))
-    },
-    cursorPos(value) {
-      if (!this.cursorTimer) { return }
-      this.$nextTick(() => {
-        let cursor = this.$el.querySelector('i')
-        if (this.cursorPos) {
-          let charactor = this.$el.querySelector(`span:nth-of-type(${this.cursorPos})`)
-          cursor.style.left = charactor.offsetLeft + charactor.offsetWidth + 'px'
-        }
-        else {
-          cursor.style.left = 0
-        }
-      })
     }
   },
   methods: {
-    toValue(rawValue) {
-      let value = rawValue.join('')
-      return value && this.type === 'number' ? parseFloat(value, 10) : value
-    },
-    openKeyboard() {
-      if (this._keyboard) { return }
-      let keyboard = new Vue({
-        render: h => h(Keyboard, {
-          props: Object.assign({
-            layout: this.type
-          }, this.keyboard),
-          on: {
-            press: key => {
-              this.input(key)
-            }
-          }
-        }),
-        destroyed() {
-          document.body.removeChild(container)
-        }
+    setCursorPos() {
+      this.$forceUpdate()
+      this.$nextTick(() => {
+        Mixins.setCursorPos.call(this)
       })
-      keyboard.$mount()
-      this._keyboard = keyboard
-      let container = document.createElement('div')
-      let shadow = document.createElement('div')
-      let element = keyboard.$el
-      let borderColor = keyboard.$el.querySelector('td').style.borderColor
-      container.style.cssText = 'position:fixed; bottom:0; left:0; width:100%; height:36%;'
-      shadow.style.cssText = 'height:100%;'
-      element.style.cssText = `position:absolute; top:0; left:0; transform: translateY(100%); border-top: 1px solid ${borderColor}; box-shadow: 0 2px 2px 2px ${borderColor}`
-      container.appendChild(shadow)
-      container.appendChild(element)
-      document.body.appendChild(container)
-      animation((timestamp, frame, frames) => {
-        element.style.transform = `translateY(${(frames - frame) / frames * 100}%)`
-      }, () => {}, 10)
-      keyboardCenter.open(this)
-      this.cursorPos = this.rawValue.length
-      this.cursorTimer = window.setInterval(() => {
-        this.cursorVisible = !this.cursorVisible
-      }, 500)
-      this.$emit('focus')
     },
-    closeKeyboard() {
-      if (!this._keyboard) { return }
-      let keyboard = this._keyboard
-      let element = keyboard.$el
-      this._keyboard = null
-      animation((timestamp, frame, frames) => {
-        element.style.transform = `translateY(${frame / frames * 100}%)`
-      }, () => {
-        keyboard.$destroy()
-      }, 10)
-      keyboardCenter.close()
-      this.cursorPos = 0
-      window.clearInterval(this.cursorTimer)
-      this.cursorTimer = null
-      this.$emit('blur')
+    createKeyboard(el, options, callback) {
+      return new Vue({
+        el,
+        render: h => h(Keyboard, {
+          props: options,
+          on: {
+            press: callback
+          }
+        })
+      })
     },
-    focus(e) {
-      e.stopPropagation()
-      this.openKeyboard()
-      if (this.cursorTimer) {
-        this.cursorPos = +e.target.dataset.index || this.rawValue.length
-      }
+    destroyKeyboard(keyboardClass) {
+      keyboardClass.$destroy()
     },
-    input(key) {
-      let value = this.rawValue.slice()
-      switch (key) {
-        case 'esc':
-        case 'enter':
-          this.closeKeyboard()
-          break
-        case 'del':
-          if (this.cursorPos > 0) {
-            value.splice(this.cursorPos - 1, 1)
-            if (this._validate(value)) {
-              this.rawValue = value
-              this.cursorPos -= 1
-            }
-          }
-          break
-        case '.':
-          if (value && value.indexOf(key) === -1) {
-            value.splice(this.cursorPos, 0, key)
-            if (this._validate(value)) {
-              this.rawValue = value
-              this.cursorPos += 1
-            }
-          }
-          break
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-          if (this.type === 'number' || typeof this.maxlength === 'undefined' || value.length < this.maxlength) {
-            value.splice(this.cursorPos, 0, key)
-            if (this._validate(value)) {
-              this.rawValue = value
-              this.cursorPos += 1
-            }
-          }
-          break
-      }
+    dispatch(event, ...args) {
+      this.$emit(event, ...args)
     }
   }
 }
 
 </script>
 
-<style lang="stylus">
-
-.numeric-input
-  display inline-block
-  background white
-  width 12em
-  height 1.2em
-  padding 2px
-  text-align left
-
-  &.readonly, &.disabled
-    opacity 0.5
-    pointer-events none
-
-  div
-    position relative
-    height 100%
-
-  span
-    float left
-    height 100%
-    display table-cell
-    vertical-align middle
-
-  i
-    pointer-events none
-    position absolute
-    left 0
-    top 0
-    bottom 0
-    width 1px
-
-.numeric-input-placeholder
-  color #757575
-
-</style>
+<style lang="stylus" src="lib/style/input.styl"></style>
